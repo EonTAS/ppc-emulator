2036f528eb2eb4c266cf87110e5b6489400a7fd9
Martin Oglesby <oglesby.mo@gmail.com>
2021-05-06 23:05:20 +0000

    created a getBit and a split to the bitfield object

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,20 @@
 var computer;
 class Bitfield {
     constructor(value, size) {
+        
         this.values = []
         this.size = size
         for (let i = 0; i < size; i++) {
             this.values[size-i-1] = Boolean(value & (1<<i))
         }
     }
-    getValue() {
-        return this.getValue(0, this.values.length-1)
+    setValue(value) {
+        for (let i = 0; i < this.size; i++) {
+            this.values[size-i-1] = Boolean(value & (1<<i))
+        }
+    }
+    getFullValue() {
+        return this.getValue(0, this.size)
     }
     getValue(start,size) {
         let val = 0
@@ -18,7 +24,25 @@ class Bitfield {
         return val
     }
     getSubField(start, size) {
-        return new Bitfield(getValue(start, size), size)
+        return new Bitfield(this.getValue(start, size), size)
+    }
+    split(splits) {
+        let returnSplits = []
+        let prevSplit
+        for(let i = 0; i < splits.length; i++) {
+            let split = splits[i]
+            if(prevSplit != undefined) {
+                console.log(prevSplit + " " + (split - prevSplit))
+                returnSplits.push(this.getSubField(prevSplit, split-prevSplit))
+            }
+            prevSplit = split
+        }
+        console.log(prevSplit + " " + (this.size - prevSplit))
+        returnSplits.push(this.getSubField(prevSplit, this.size-prevSplit))
+        return returnSplits
+    }
+    getBit(i) {
+        return this.values[i]
     }
 }
 
@@ -66,6 +90,8 @@ class Computer {
         this.lr = 0
         this.ctr = 0
 
+        this.CR = new Bitfield(0, 32)
+
         
         for (let i = 0; i < program.length; i++) {
             this.memory.setWord(parseInt(program[i], 16), 4*i)
@@ -79,47 +105,76 @@ class Computer {
         this.count = 0
         while (this.programCounter < program.length*4) {
             console.log("PC = " + this.programCounter)
-            this.step(this.memory.getWord(this.programCounter))
+            let instruction = new Bitfield(this.memory.getWord(this.programCounter), 32)
+            this.step(instruction)
             if(this.count++ > maxLoop) {
                 break
             }
         }
         console.log(this)
     }
-    init() {
-
-    }
     step(instruction) {
-        console.log("   current instruction : " + instruction.toString(16))
-        let opcode = maskBits(instruction, 0, 6)
+        console.log("   current instruction : " + instruction.getFullValue().toString(16))
+        let opcode = instruction.getValue(0,6)
         let CIA = this.programCounter //current instruction address
         let NIA = CIA + 0x4 //next instruction address
         let args;
         switch(opcode) {
-            case 18: //branch
+            case 18: {//branch 
+                console.log("   branch")
                 args = this.getArgs(instruction, "I")
-                if (args["AA"]) {
-                    NIA = EXTS(args["LI"]<<2,24)
+                if (args["AA"].getFullValue()) {
+                    NIA = EXTS((args["LI"].getFullValue())<<2,24)
                 } else {
-                    NIA = CIA + EXTS(args["LI"]<<2, 24)
+                    NIA = CIA + EXTS((args["LI"].getFullValue())<<2, 24)
                 }
                 console.log("   branch to 0x" + NIA.toString(16))
-                if (args["LK"]) {
+                if (args["LK"].getFullValue()) {
                     this.lr = CIA + 4
                     console.log("   set lr to 0x" + this.lr.toString(16))
                 } 
-                break
-            case 16: //branch conditional
+                break 
+            }
+            case 16: {//branch conditional
+                console.log("   branch conditional")
                 args = this.getArgs(instruction, "B")
-                if (!args["BO"]) {
+                if (!args["BO"].getBit(2)) {
                     this.ctr = this.ctr-1
                 }
-                
-                if (args["LK"]) {
-                    this.lr = CIA + 4
-                    console.log("   set lr to 0x" + this.lr.toString(16))
-                } 
+                let ctr_ok = (args["BO"].getBit(2)) | ((this.ctr != 0 ) ^ args["BO"].getBit(3)) 
+                let cond_ok = (args["BO"].getBit(0)) | (this.CR.getBit(args["BI"].getFullValue()) == args["BO"].getBit(1))
+                if (ctr_ok && cond_ok) {
+                    
+                    if (args["AA"].getFullValue()) {
+                        NIA = EXTS((args["BD"].getFullValue())<<2,16)
+                    } else {
+                        NIA = CIA + EXTS((args["BD"].getFullValue())<<2, 16)
+                    }
+                    console.log("   conditional branch to 0x" + NIA.toString(16))
+                    if (args["LK"].getFullValue()) {
+                        this.lr = CIA + 4
+                        console.log("   set lr to 0x" + this.lr.toString(16))
+                    } 
+                }
                 break
+            }
+            case 19: { //branch conditional to count register
+                let cond_ok = (args["BO"].getBit(0)) | (this.CR.getBit(args["BI"].getFullValue()) == args["BO"].getBit(1))
+                if (cond_ok) {
+                    let type = args["BD"].getValue(5,10)
+                    if (type == 528){
+                        NIA = this.ctr<<2
+                    } else if (type == 16) {
+                        NIA = this.lr<<2
+                    }
+                    console.log("   conditional branch to count register 0x" + this.ctr.toString(16))
+                    if (args["LK"].getFullValue()) {
+                        this.lr = CIA + 4
+                        console.log("   set lr to 0x" + this.lr.toString(16))
+                    } 
+                }
+                break
+            }
             default: 
                 console.log("   unknown instruction : " + opcode)
                 break
@@ -131,22 +186,23 @@ class Computer {
         let returnStruct = {}
         switch(type) {
             case "I":
-                returnStruct["LI"] = maskBits(instruction, 6, 24)
-                returnStruct["AA"] = maskBits(instruction, 30, 1)
-                returnStruct["LK"] = maskBits(instruction, 31, 1)
+                returnStruct["LI"] = instruction.getSubField(6, 25)
+                returnStruct["AA"] = instruction.getSubField(30, 1)
+                returnStruct["LK"] = instruction.getSubField(31, 1)
                 break
             case "B":
-                returnStruct["BO"] = maskBits(instruction, 6, 5)
-                returnStruct["BI"] = maskBits(instruction, 11, 5)
-                returnStruct["BD"] = maskBits(instruction, 16, 14)
-                returnStruct["AA"] = maskBits(instruction, 30, 1)
-                returnStruct["LK"] = maskBits(instruction, 31, 1)
+                returnStruct["BO"] = instruction.getSubField(6, 5)
+                returnStruct["BI"] = instruction.getSubField(11, 5)
+                returnStruct["BD"] = instruction.getSubField(16, 14)
+                returnStruct["AA"] = instruction.getSubField(30, 1)
+                returnStruct["LK"] = instruction.getSubField(31, 1)
                 break
             default: 
                 return null;
         }
         return returnStruct
     }
+
 }
 function EXTS(val, originalSize) {
     
@@ -157,17 +213,7 @@ function EXTS(val, originalSize) {
     return val
 
 }
-function maskBits(val, start, len) {
-     let mask = (1<<(len)) - 1   
-     mask = mask << (32-start-len)
-     val &= mask 
-     return val >>> (32-start-len)
-}
 
-
-function interpret(code) {
-
-}
 
 function restartCPU() {
     text = $("#input").val().replace(/[^A-Fa-f0-9]/g, "");
